1010dc1bd0a7d9bed532f56de2e6641a19dfbd09
MartinOfi <martin@tqr.la>
2022-09-14 11:14:46 -0300

    first init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //selector: "nombre de la etiqueta"
+  selector: 'app-root',
+  //Template Url: es lo que se va a mostrar con esa etiqueta
+  templateUrl: './app.component.html',
+  //stylesUrl: "es el estilo del componente"
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Ricardo';
+}
