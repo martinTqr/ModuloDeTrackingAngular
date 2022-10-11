62b308251a533a95590ebcd030fa5468645bf879
MartinOfi <martin@tqr.la>
2022-10-11 12:18:15 -0300

    borrado groid componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxPivotGridModule } from 'devextreme-angular';
 import { AppComponent } from './components/app/app.component';
 import { BannerComponent } from './components/banner';
-import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
-  declarations: [AppComponent, BannerComponent, GridComponent],
-  imports: [BrowserModule, DxPivotGridModule],
+  declarations: [AppComponent, BannerComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
