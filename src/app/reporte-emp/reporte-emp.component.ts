ebf158a3348a52e4bda71631f637429e3271201c
MartinOfi <martin@tqr.la>
2022-10-28 15:30:46 -0300

    borrados comentarios, y ahora aparecen las cajas

--- src/app/reporte-emp/reporte-emp.component.ts
+++ src/app/reporte-emp/reporte-emp.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { meses } from '../constantes/';
+import { Reporte } from '../interfaces';
 import { ReporteEmpresaService } from './reporte-emp.service';
 
 @Component({
@@ -13,13 +14,18 @@ export class ReporteEmpComponent implements OnInit {
   nombreDeMeses: string[] = meses;
   constructor(servicio: ReporteEmpresaService) {
     const reportes = servicio.buscarReporteEmpresa();
-    this.reportes = reportes;
-
-    /* reportes.map((reporte: Reporte) => ({
-      ...reporte,
-      subcategorias: [...reporte.subcategorias, ...reporte.cajas], 
-    }));*/
+    this.reportes = reportes.map((reporte: Reporte) => {
+      const cajas = reporte.cajas.map((caja: any) => ({
+        ...caja,
+        acumulado: {
+          total: caja.total,
+        },
+      }));
+      return {
+        ...reporte,
+        subcategorias: [...reporte.subcategorias, ...cajas],
+      };
+    });
   }
-
   ngOnInit(): void {}
 }
