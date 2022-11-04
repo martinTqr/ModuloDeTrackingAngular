e07e7348df3d5dd6466e33c298c792688b4647e9
MartinOfi <martin@tqr.la>
2022-11-04 16:53:00 -0300

    detalle de empresa listo

--- src/app/empresa/detalle-empresa.component.ts
+++ src/app/empresa/detalle-empresa.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Empresa } from '../models';
+import { EmpresaService } from '../services/empresa.service';
+import swal from 'sweetalert';
 @Component({
   selector: 'app-detalle-empresa',
   templateUrl: './detalle-empresa.component.html',
-  styleUrls: ['./detalle-empresa.component.css']
+  styleUrls: ['./detalle-empresa.component.css'],
 })
 export class DetalleEmpresaComponent implements OnInit {
-
-  constructor() { }
+  empresa!: Empresa;
+  constructor(
+    private empresaService: EmpresaService,
+    private rutaActiva: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const { id } = this.rutaActiva.snapshot.params;
+    this.empresaService.detalle(id).subscribe(
+      (empresa) => (this.empresa = empresa),
+      ({ error }) =>
+        swal({
+          icon: 'error',
+          title: 'Error',
+          text: error.message,
+        })
+    );
   }
-
 }
