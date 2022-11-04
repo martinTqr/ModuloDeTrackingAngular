167c5e47d7f2e03ceeea3a775816a3b2508bc268
MartinOfi <martin@tqr.la>
2022-11-04 11:11:30 -0300

    termimnado las 3 pantallas de movimientos

--- src/app/movimiento/detalle-movimiento.component.ts
+++ src/app/movimiento/detalle-movimiento.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Movimiento } from '../models';
+import { MovimientoService } from '../services/movimiento.service';
 
 @Component({
   selector: 'app-detalle-movimiento',
   templateUrl: './detalle-movimiento.component.html',
-  styleUrls: ['./detalle-movimiento.component.css']
+  styleUrls: ['./detalle-movimiento.component.css'],
 })
 export class DetalleMovimientoComponent implements OnInit {
-
-  constructor() { }
+  movimiento!: Movimiento;
+  constructor(private movimientoService: MovimientoService) {}
 
   ngOnInit(): void {
+    this.movimientoService.detalle(12).subscribe(
+      (movimiento) => (this.movimiento = movimiento),
+      ({ error }) => console.error(error)
+    );
   }
-
 }
