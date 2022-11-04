464eaed2b33f723a60a3d7f72445b11c29271bd7
MartinOfi <martin@tqr.la>
2022-11-04 11:24:37 -0300

    borrada caja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { DxButtonModule, DxTreeListModule } from 'devextreme-angular';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { CajaComponent } from './caja/caja.component';
 import { ReporteUNComponent } from './reporte-un/reporte-un.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
@@ -23,7 +22,6 @@ if (!/localhost/.test(document.location.host)) {
     AppComponent,
     ReporteUNComponent,
     ReporteEmpComponent,
-    CajaComponent,
     InicioComponent,
     ListaMovimientoComponent,
     NuevoMovimientoComponent,
