521b3e936552c8d0e159104e6338481130855a3b
MartinOfi <martin@tqr.la>
2022-11-04 17:40:18 -0300

    detalle categoria listo

--- src/app/categoria/detalle-categoria.component.ts
+++ src/app/categoria/detalle-categoria.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Categoria } from '../models';
+import { CategoriaService } from '../services/categoria.service';
 
 @Component({
   selector: 'app-detalle-categoria',
   templateUrl: './detalle-categoria.component.html',
-  styleUrls: ['./detalle-categoria.component.css']
+  styleUrls: ['./detalle-categoria.component.css'],
 })
 export class DetalleCategoriaComponent implements OnInit {
-
-  constructor() { }
+  categoria!: Categoria;
+  constructor(
+    private categoriaService: CategoriaService,
+    private rutaActiva: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const { id } = this.rutaActiva.snapshot.params;
+    this.categoriaService.detalle(id).subscribe(
+      (categoria) => (this.categoria = categoria),
+      (err) => console.log(err)
+    );
   }
-
 }
