7f09bf80eb0e17da3d381691afb3617c626bbb07
MartinOfi <martin@tqr.la>
2022-11-09 12:17:19 -0300

    Creacion unidad de negocio funcionando

--- src/app/models/unidad-negocio.ts
+++ src/app/models/unidad-negocio.ts
@@ -1,7 +1,7 @@
 export class UnidadNegocio {
   id?: number;
   nombre: string;
-  idEmpresa: string;
+  idEmpresa: string | number;
   fechaBorrado?: number;
   constructor({ id, nombre, idEmpresa, fechaBorrado }: any) {
     this.id = id;
--- src/app/unidad-negocio/nueva-unidad-negocio.component.ts
+++ src/app/unidad-negocio/nueva-unidad-negocio.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { UnidadNegocioService } from '../services/unidad-negocio.service';
 
 @Component({
   selector: 'app-nueva-unidad-negocio',
   templateUrl: './nueva-unidad-negocio.component.html',
-  styleUrls: ['./nueva-unidad-negocio.component.css']
+  styleUrls: ['./nueva-unidad-negocio.component.css'],
 })
 export class NuevaUnidadNegocioComponent implements OnInit {
+  unidadNegocioFormulario = this.fb.group({
+    nombre: ['', Validators.required],
+    idEmpresa: ['11', Validators.required],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private unidadNegocioService: UnidadNegocioService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  crear() {
+    let { nombre, idEmpresa } = this.unidadNegocioFormulario.value;
+    nombre = String(nombre);
+    const idEmpresaNumber = Number(idEmpresa);
+    this.unidadNegocioService
+      .crear({ nombre, idEmpresa: idEmpresaNumber })
+      .subscribe(
+        () =>
+          Swal.fire({
+            title: 'Unidad de negocio creada',
+            icon: 'success',
+          }),
+        ({ error }) => {
+          Swal.fire({
+            title: 'Error al crear unidad de negocio',
+            icon: 'error',
+            text: error.message,
+          });
+        }
+      );
   }
-
 }
