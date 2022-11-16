ee20b02818cff2b1c38d780c08d51a89ce957266
MartinOfi <martin@tqr.la>
2022-11-16 16:37:13 -0300

    quitada relacion de caja con unidad de negocio

--- src/app/caja/nueva-caja.component.ts
+++ src/app/caja/nueva-caja.component.ts
@@ -14,29 +14,16 @@ import { UnidadNegocioService } from '../services/unidad-negocio.service';
 export class NuevaCajaComponent implements OnInit {
   cajaFormulario = this.fb.group({
     nombre: ['', Validators.required],
-    idUnidadNegocio: ['', Validators.required],
     negativa: ['', Validators.required],
   });
   unidadesNegocio: UnidadNegocio[] = [];
 
-  constructor(
-    private fb: FormBuilder,
-    private cajaService: CajaService,
-    private unidadNegocioService: UnidadNegocioService
-  ) {}
+  constructor(private fb: FormBuilder, private cajaService: CajaService) {}
 
-  ngOnInit(): void {
-    this.cargarUnidadesNegocio();
-  }
-  cargarUnidadesNegocio() {
-    this.unidadNegocioService
-      .lista()
-      .subscribe((unidades) => (this.unidadesNegocio = unidades));
-  }
+  ngOnInit(): void {}
   crear(): void {
     if (this.cajaFormulario.invalid) return;
-    let { idUnidadNegocio, negativa, nombre } = this.cajaFormulario.value;
-    const idUnidadNegocioNumber = Number(idUnidadNegocio);
+    let { negativa, nombre } = this.cajaFormulario.value;
     const negativaBoolean = trasnformarABoolean(negativa);
     nombre = transformarAString(nombre);
 
@@ -44,7 +31,6 @@ export class NuevaCajaComponent implements OnInit {
       .crear({
         nombre,
         negativa: negativaBoolean,
-        idUnidadNegocio: idUnidadNegocioNumber,
       })
       .subscribe(
         ({ data }) =>
--- src/app/models/caja.ts
+++ src/app/models/caja.ts
@@ -1,13 +1,11 @@
 export class Caja {
   id?: number;
   nombre: string;
-  idUnidadNegocio: number;
   negativa: boolean;
   fechaBorrado?: number;
-  constructor({ id, nombre, idUnidadNegocio, negativa, fechaBorrado }: any) {
+  constructor({ id, nombre, negativa, fechaBorrado }: any) {
     this.id = id;
     this.nombre = nombre;
-    this.idUnidadNegocio = idUnidadNegocio;
     this.negativa = negativa;
     this.fechaBorrado = fechaBorrado;
   }
