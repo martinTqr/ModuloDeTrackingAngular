3eb6fcdd7702e869fbf1ecc907227e24615bbe72
MartinOfi <martin@tqr.la>
2022-11-17 18:13:57 -0300

    creacion de cajas con grupo de cajas funcionando

--- src/app/caja/nueva-caja.component.ts
+++ src/app/caja/nueva-caja.component.ts
@@ -3,9 +3,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { CajaService } from '../services/caja.service';
 import swal from 'sweetalert2';
 import { transformarAString } from '../helper';
-import { UnidadNegocio } from '../models';
+import { Caja, GrupoCaja, UnidadNegocio } from '../models';
 import { UnidadNegocioService } from '../services/unidad-negocio.service';
 import { EmpresaService } from '../services/empresa.service';
+import { GrupoCajaService } from '../services/grupo-caja.service';
 
 @Component({
   selector: 'app-nueva-caja',
@@ -16,20 +17,27 @@ export class NuevaCajaComponent implements OnInit {
   cajaFormulario = this.fb.group({
     idEmpresa: ['', [Validators.required]],
     nombre: ['', Validators.required],
+    idGrupoCaja: ['', Validators.required],
     negativa: ['', Validators.required],
   });
-  unidadesNegocio: UnidadNegocio[] = [];
-
+  grupoCajas: GrupoCaja[] = [];
   constructor(
     private fb: FormBuilder,
     private cajaService: CajaService,
-    private empresaService: EmpresaService
+    private empresaService: EmpresaService,
+    private grupoCajaService: GrupoCajaService
   ) {}
 
   ngOnInit(): void {
     this.cargarEmpresa();
+    this.cargarGrupoDeCajas();
   }
 
+  cargarGrupoDeCajas() {
+    this.grupoCajaService
+      .lista()
+      .subscribe((grupoCajas) => (this.grupoCajas = grupoCajas));
+  }
   cargarEmpresa() {
     this.empresaService.lista().subscribe((empresas) => {
       this.cajaFormulario.patchValue({ idEmpresa: String(empresas[0].id) });
@@ -37,34 +45,35 @@ export class NuevaCajaComponent implements OnInit {
   }
   crear(): void {
     if (this.cajaFormulario.invalid) return;
-    let { idEmpresa, negativa, nombre } = this.cajaFormulario.value;
+    let { idEmpresa, idGrupoCaja, negativa, nombre } =
+      this.cajaFormulario.value;
     const idEmpresaNumber = Number(idEmpresa);
     const negativaBoolean = trasnformarABoolean(negativa);
+    const idGrupoCajaNumber = Number(idGrupoCaja);
     nombre = transformarAString(nombre);
+    const caja: Caja = {
+      idEmpresa: idEmpresaNumber,
+      nombre,
+      idGrupoCaja: idGrupoCajaNumber,
+      negativa: negativaBoolean,
+    };
+    this.cajaService.crear(caja).subscribe(
+      ({ data }) =>
+        swal.fire({
+          title: 'Caja creada',
+          text: `La caja ${data.nombre} ha sido creada con éxito`,
+          icon: 'success',
+        }),
+      (error) => {
+        console.log(error);
 
-    this.cajaService
-      .crear({
-        idEmpresa: idEmpresaNumber,
-        nombre,
-        negativa: negativaBoolean,
-      })
-      .subscribe(
-        ({ data }) =>
-          swal.fire({
-            title: 'Caja creada',
-            text: `La caja ${data.nombre} ha sido creada con éxito`,
-            icon: 'success',
-          }),
-        (error) => {
-          console.log(error);
-
-          swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: error.message.map((mensaje: string) => mensaje).join(' '),
-          });
-        }
-      );
+        swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: error.message.map((mensaje: string) => mensaje).join(' '),
+        });
+      }
+    );
   }
 }
 const trasnformarABoolean = (value: any): boolean => {
--- src/app/models/caja.ts
+++ src/app/models/caja.ts
@@ -1,12 +1,21 @@
 export class Caja {
   id?: number;
   idEmpresa: number;
+  idGrupoCaja: number;
   nombre: string;
   negativa: boolean;
   fechaBorrado?: number;
-  constructor({ id, nombre, idEmpresa, negativa, fechaBorrado }: any) {
+  constructor({
+    id,
+    nombre,
+    idEmpresa,
+    idGrupoCaja,
+    negativa,
+    fechaBorrado,
+  }: any) {
     this.id = id;
     this.idEmpresa = idEmpresa;
+    this.idGrupoCaja = idGrupoCaja;
     this.nombre = nombre;
     this.negativa = negativa;
     this.fechaBorrado = fechaBorrado;
