09d05487befeadb500dfb8397a70bccf2607d519
MartinOfi <martin@tqr.la>
2022-11-23 11:11:57 -0300

    creada la lista de empresas en el home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresa } from '../models';
+import { EmpresaService } from '../services/empresa.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  empresas: Empresa[] = [];
+  constructor(private empresaService: EmpresaService) {}
 
   ngOnInit(): void {
+    this.cargarEmpresas();
   }
 
+  cargarEmpresas() {
+    this.empresaService.lista().subscribe((empresas) => {
+      this.empresas = empresas;
+    });
+  }
 }
