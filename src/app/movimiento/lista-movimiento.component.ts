ebc0d78279a91e84215d5bc68a6527eef74b3a03
MartinOfi <martin@tqr.la>
2022-12-06 16:54:13 -0300

    filtro de cajas funcionando

--- src/app/movimiento/lista-movimiento.component.ts
+++ src/app/movimiento/lista-movimiento.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { parsearFecha, separarMiles } from '../helper';
-import { Categoria, Movimiento } from '../models';
+import { Caja, Categoria, Movimiento } from '../models';
+import { CajaService } from '../services/caja.service';
 import { MovimientoService } from '../services/movimiento.service';
 
 @Component({
@@ -12,6 +13,7 @@ import { MovimientoService } from '../services/movimiento.service';
 })
 export class ListaMovimientoComponent implements OnInit {
   movimientosForm = this.fb.group({
+    idCaja: [''],
     fechaInicio: [''],
     fechaFin: [''],
   });
@@ -24,22 +26,67 @@ export class ListaMovimientoComponent implements OnInit {
     tipo: '',
   };
   movimientos: Movimiento[] = [];
-  categorias: Categoria[] = [];
+  cajas: Caja[] = [];
   constructor(
     private movimientoService: MovimientoService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private cajaService: CajaService
   ) {}
 
   ngOnInit(): void {
     this.cargarMovimientos();
+    this.cargarCajas();
   }
-
+  cargarCajas() {
+    this.cajaService.lista().subscribe((cajas) => {
+      this.cajas = cajas;
+    });
+  }
   filtrarMovimientos() {
-    const { fechaInicio, fechaFin } = this.movimientosForm.value;
+    const { fechaInicio, fechaFin, idCaja } = this.movimientosForm.value;
     const fechaInicioDate = fechaInicio ? new Date(fechaInicio!) : undefined;
     const fechaFinDate = fechaFin ? new Date(fechaFin!) : undefined;
-    this.cargarMovimientos(fechaInicioDate, fechaFinDate);
+    this.cargarMovimientos(fechaInicioDate, fechaFinDate, [idCaja]);
   }
+
+  cargarMovimientos(fechaInicio?: Date, fechaFin?: Date, cajas?: any[]) {
+    this.movimientoService
+      .lista({ fechaInicio, fechaFin, idCajas: cajas })
+      .subscribe(
+        ({ movimientos }) =>
+          (this.movimientos = movimientos.sort(
+            (a: any, b: any) => b.id - a.id
+          )),
+        (error) => console.error(error)
+      );
+  }
+
+  borrar(id: any): void {
+    this.movimientoService.borrar(id).subscribe(
+      () => {
+        this.movimientos = this.movimientos.filter(
+          (movimiento) => movimiento.id !== id
+        );
+        Swal.fire(
+          'Movimiento borrado',
+          `Movimiento ${id} eliminado con éxito`,
+          'success'
+        );
+      },
+
+      ({ error }) => {
+        Swal.fire('Error!', error.mensaje || error.message, 'error');
+      }
+    );
+  }
+
+  parsearFecha(fecha: string, formato?: string): string {
+    return parsearFecha(fecha, formato);
+  }
+  separarMiles(numero: number): string {
+    return separarMiles(numero);
+  }
+
   ordenarCampo(campo?: string) {
     if (this.orden[campo] === '') {
       this.movimientos.sort((a: any, b: any) => {
@@ -58,37 +105,5 @@ export class ListaMovimientoComponent implements OnInit {
       this.movimientos.sort((a: any, b: any) => a.monto - b.monto);
       this.orden.monto = 'cambio';
     } else this.movimientos.sort().reverse();
-  }
-
-  cargarMovimientos(fechaInicio?: Date, fechaFin?: Date) {
-    this.movimientoService.lista({ fechaInicio, fechaFin }).subscribe(
-      ({ movimientos }) =>
-        (this.movimientos = movimientos.sort((a: any, b: any) => b.id - a.id)),
-      (error) => console.error(error)
-    );
-  }
-  parsearFecha(fecha: string, formato?: string): string {
-    return parsearFecha(fecha, formato);
-  }
-  separarMiles(numero: number): string {
-    return separarMiles(numero);
-  }
-  borrar(id: any): void {
-    this.movimientoService.borrar(id).subscribe(
-      () => {
-        this.movimientos = this.movimientos.filter(
-          (movimiento) => movimiento.id !== id
-        );
-        Swal.fire(
-          'Movimiento borrado',
-          `Movimiento ${id} eliminado con éxito`,
-          'success'
-        );
-      },
-
-      ({ error }) => {
-        Swal.fire('Error!', error.mensaje || error.message, 'error');
-      }
-    );
   }
 }
